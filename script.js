db081dd065badfe04142695e77093a48aaabcd3e
Nick Woods <nicholas.andrew.woods@gmail.com>
2018-01-15 19:52:11 -0800

    fretboard indicator now works

--- script.js
+++ script.js
@@ -1,50 +1,93 @@
 function newNote() {
-  noteNames = { 0:  ["C", "B sharp"],
-	        1:  ["C sharp", "D flat"],
-		2:  ["D", "D"],
-		3:  ["D sharp", "E flat"],
-		4:  ["E", "F flat"],
-		5:  ["F", "E sharp"],
-	        6:  ["F sharp", "G flat"],
-		7:  ["G", "G"],
-		8:  ["G sharp", "A flat"],
-		9:  ["A", "A"],
-		10: ["A sharp", "B flat"],
-		11: ["B", "C flat"]
+  var noteNames = { 
+    0:  ["C", "B sharp"],
+    1:  ["C sharp", "D flat"],
+    2:  ["D", "D"],
+    3:  ["D sharp", "E flat"],
+    4:  ["E", "F flat"],
+    5:  ["F", "E sharp"],
+    6:  ["F sharp", "G flat"],
+    7:  ["G", "G"],
+    8:  ["G sharp", "A flat"],
+    9:  ["A", "A"],
+    10: ["A sharp", "B flat"],
+    11: ["B", "C flat"]
   };
-  stringNames = { 1: ["High E"],
-	          2: ["B"],
-		  3: ["G"],
-		  4: ["D"],
-		  5: ["A"],
-		  6: ["Low E"]
+  var stringNames = { 
+    1: ["High E"],
+    2: ["B"],
+    3: ["G"],
+    4: ["D"],
+    5: ["A"],
+    6: ["Low E"]
   }; 
-  dotLocsX = { 0:  "591px";
-  	       1:  "525px";
-               2:  "473px";
-               3:  "414px";
-               4:  "359px";
-               5:  "306px";
-               6:  "257px";
-               7:  "211px";
-               8:  "168px";
-               9:  "126px";
-               10: "87px";
-               11: "50px"
+  var stringNotes = {
+    1: 4,
+    2: 11,
+    3: 7,
+    4: 2,
+    5: 9,
+    6: 4
   };
-  dotLocsY = { 1: "81px";
-	       2: "67px";
-	       3: "52px";
-	       4: "37px";
-	       5: "22px";
-	       6: "8px";
+  var dotLocsX = {
+    0:  "591px",
+    1:  "535px",
+    2:  "473px",
+    3:  "414px",
+    4:  "359px",
+    5:  "306px",
+    6:  "257px",
+    7:  "211px",
+    8:  "168px",
+    9:  "126px",
+    10: "87px",
+    11: "50px"
+  };
+  var dotLocsY = {
+    1: "81px",
+    2: "67px",
+    3: "52px",
+    4: "37px",
+    5: "22px",
+    6: "8px"
+  };
 	      
-  note = Math.floor(Math.random() * 12);
-  string = Math.floor(Math.random() * 6) + 1;
+  var note = Math.floor(Math.random() * 12);
+  console.log(note);
+  var string = Math.floor(Math.random() * 6) + 1;
+  console.log(string);
+  console.log(stringNotes[string]);
+  var fret = (note - stringNotes[string]) % 12;
+  if (fret < 0) { fret = fret + 12 };
+  console.log(fret);
+  var dotLocX = dotLocsX[fret];
+  var dotLocY = dotLocsY[string];
+
   $("#note").html("Note: " + noteNames[note][Math.floor(Math.random() * 2)]);
   $("#string").html("String: " + stringNames[string]);
-}
+
+  $("#greendot").css("opacity", 0);
+  $("#greendot").css("left", dotLocX);
+  $("#greendot").css("top", dotLocY);
+};
   
 $(document).ready(function() {
-  $("#newnote").on("click", newNote);
+  var dotVisibility = 0;
+  $("#newnote").on("click", function() {
+    dotVisibility = 0;
+    $("#dottoggle").html("Show");
+    newNote();
+  });
+  $("#dottoggle").on("click", function() {
+    if (dotVisibility === 1) {
+      dotVisibility = 0;
+      $("#greendot").css("opacity", 0);
+      $("#dottoggle").html("Show");
+    } else {
+      dotVisibility = 1;
+      $("#greendot").css("opacity", 1);
+      $("#dottoggle").html("Hide");
+    };
+  });
+      
 });
